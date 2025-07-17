3da814ee83f74e69081f4ba14f218740e95f6669
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-17 16:22:58 +0200

    removed workflow comments on reached tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-//*crea i componenti da riutilizzare (button?)
-//*importa i componenti da utilizzare [header, main(lista di bottoni), textboxes]
 //*enable useState functionality
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Btn from "./components/btns/Btn";
 
-//*centralizza i dati dell'array
 import codLan from "./data/codingLanguages";
 
 function App() {
